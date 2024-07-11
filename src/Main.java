1b7692f657b7810ed486e31998c21fda29e6ca39
Dmitrii Nasedkin <2_tone@mail.ru>
2024-07-11 16:37:23 +0300

    Создана новая ветка "2-number-experiments". Написан код принимающий на вход два целых числа и рассчитывающий сумму, разность, произведение и частное этих чисел.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        double qoutient = (double) firstNumber / secondNumber;
+
+        System.out.println("Сумма чисел: " + (firstNumber+secondNumber));
+        System.out.println("Разность чисел: " + (firstNumber-secondNumber));
+        System.out.println("Произведение чисел: " + (firstNumber*secondNumber));
+        System.out.println("Частное чисел: " + qoutient);
     }
 }
