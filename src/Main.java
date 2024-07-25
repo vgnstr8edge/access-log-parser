3d2ca3539e9b0b75d25ff1c8fee2fffe5b032d01
Dmitrii Nasedkin <2_tone@mail.ru>
2024-07-25 15:10:35 +0300

    В методе main написан бесконечный цикл, который запрашивает у пользователя путь к файлу и проверяет его корректность и подсчитывает кол-во номеров запрошенных файлов. В случае указания пути к папке или некорректного пути - выводится соответствующее сообщение об этом.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+
+            if (isDirectory) {
+                System.out.println("Данный путь ведет к папке. Повторите ввод.");
+                continue;
+            }
+
+            if (fileExists) {
+                count += 1;
+                System.out.println("Путь указан верно.");
+                System.out.println("Это файл номер " + count);
+            } else {
+                System.out.println("Данный путь некорректен. Повторите ввод.");
+            }
+        }
     }
 }
