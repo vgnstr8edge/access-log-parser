a07a18f4c8d268a0d4f0c8ae9ff361d39d3b77b6
Dmitrii Nasedkin <2_tone@mail.ru>
2024-08-24 13:06:23 +0300

    Добавлен код по проверке файла лога. Он проверяет общее количество строк в файле, самую длинную строку и самую короткую. Если количество символов в строке больше 1024 - выводит исключение.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -23,10 +25,52 @@ public class Main {
                 count += 1;
                 System.out.println("Путь указан верно.");
                 System.out.println("Это файл номер " + count);
+
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader = new BufferedReader(fileReader);
+
+                    int lineNumber = 0;
+                    int maxLength = 0;
+                    int minLength = Integer.MAX_VALUE;
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        lineNumber++;
+                        int length = line.length();
+
+                        if (length > maxLength) {
+                            maxLength = length;
+                        }
+
+                        if (length < minLength) {
+                            minLength = length;
+                        }
+
+                        if (length > 1024) {
+                            throw new RuntimeException("Строка длиннее 1024 символов");
+                        }
+                    }
+
+                    System.out.println("Общее количество строк в файле: " + lineNumber);
+                    System.out.println("Длина самой длинной строки в файле: " + maxLength);
+                    System.out.println("Длина самой короткой строки в файле: " + minLength);
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
             } else {
                 System.out.println("Данный путь некорректен. Повторите ввод.");
             }
         }
+
     }
+
 }
+
+
+
+
+
 
