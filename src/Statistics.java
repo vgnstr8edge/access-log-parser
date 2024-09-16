6c9b514610d6ac8c983f3c1fcd14dfa75e9ed09d
Dmitrii Nasedkin <2_tone@mail.ru>
2024-09-16 23:26:09 +0300

    Обновил код в классе Statistic. Добавлены методы getExistingPages(), getOsStatistics() и обновлен метод addEntry().

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,15 +1,37 @@
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.HashSet;
+
 
 public class Statistics {
     int totalTraffic;
     private LocalDateTime minTime;
     private LocalDateTime maxTime;
+    private static HashSet<String> existingPages = new HashSet<>();
+    private static HashMap<String, Integer> osFrequency = new HashMap<>();
 
     public Statistics() {
         totalTraffic = 0;
         minTime = LocalDateTime.MAX;
         maxTime = LocalDateTime.MIN;
+    }
+
+    public static HashSet<String> getExistingPages() {
+        return existingPages;
+    }
+
+    public static HashMap<String, Double> getOsStatistics() {
+        HashMap<String, Double> osStatistics = new HashMap<>();
+        int totalOsCount = 0;
+        for (int count : osFrequency.values()) {
+            totalOsCount += count;
+        }
+        for (String os : osFrequency.keySet()) {
+            double ratio = (double) osFrequency.get(os) / totalOsCount;
+            osStatistics.put(os, ratio);
+        }
+        return osStatistics;
     }
 
     public void addEntry(LogEntry entry) {
@@ -20,11 +42,21 @@ public class Statistics {
         if (entry.getDateTime().isAfter(maxTime)) {
             maxTime = entry.getDateTime();
         }
+        if (entry.getResponseCode() == 200) {
+            existingPages.add(entry.getPath());
+        }
+        String os = entry.getUserAgent().getOs();
+        if (osFrequency.containsKey(os)) {
+            osFrequency.put(os, osFrequency.get(os) + 1);
+        } else {
+            osFrequency.put(os, 1);
+        }
     }
 
     public double getTrafficRate() {
         long timeDiff = ChronoUnit.HOURS.between(minTime, maxTime);
         return (double) totalTraffic / timeDiff;
     }
+
 }
 
